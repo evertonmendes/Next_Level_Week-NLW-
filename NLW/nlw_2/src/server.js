b6e5dc7901316d60256910e5bbb2b5a512b31045
evertonmendes <63795580+evertonmendes@users.noreply.github.com>
2021-02-06 19:19:48 -0300

    Add files via upload

---
+++ NLW/nlw_2/src/server.js
@@ -0,0 +1,28 @@
+//servidor
+const express= require('express')
+const server= express()
+
+const { pageLanding, pageStudy, pageGiveClasses} = require("./pages")
+
+//configurar nunjucks
+const nunjucks= require('nunjucks')
+nunjucks.configure('src/views', {
+
+    express: server,
+    noCache: true,
+
+})
+
+
+//inicio e configuração do servidor
+server
+//receber os dados do req.body
+.use(express.urlencoded({extended: true}))
+//configurar arquivos estaticos(css, scripts, images)
+.use(express.static("public"))
+//rotas
+.get("/", pageLanding) 
+.get("/study", pageStudy )
+.get("/give-classes", pageGiveClasses)
+//start do servidor
+.listen(5500)
